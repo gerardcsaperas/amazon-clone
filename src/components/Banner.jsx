8dfae90a1e96bb134f2f9e0557108d78cf419754
Gerard C. Saperas <gcsaperas@konodrac.com>
2022-08-24 17:49:04 +0200

    landing page init

---
+++ src/components/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+const Banner = () => {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showArrows={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <img loading="lazy" src="https://links.papareact.com/gi1" />
+        <img loading="lazy" src="https://links.papareact.com/6ff" />
+        <img loading="lazy" src="https://links.papareact.com/7ma" />
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Header.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../assets/logo.png";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+const Header = () => {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        {/* Logo */}
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src={logo}
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        {/* Search Bar */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow rounded-l-md focus:outline-none"
+          />
+          <SearchIcon className="h-12 p-4 cursor-pointer" />
+        </div>
+        {/* Right Side */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          {/* Account & Lists */}
+          <div className="flex-1 link ">
+            <p>Hello Gerard C. Saperas</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          {/* Returns & Orders */}
+          <div className="flex-1 link ">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          {/* Basket */}
+          <div className="flex-1 link relative flex items-center">
+            <span className="absolute top-0 lg:right-10 right-0 bg-yellow-400 h-4 w-4 text-center rounded-full text-black font-bold">
+              4
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden lg:block font-extrabold text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Bottom Nav */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
