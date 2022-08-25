561188391e717dfb341841d9343e8c28a298de59
Gerard C. Saperas <gcsaperas@konodrac.com>
2022-08-25 19:43:05 +0200

    subtotal working

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -3,11 +3,12 @@ import Header from "../components/Header";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { useDispatch, useSelector } from "react-redux";
-import { selectItems, removeFromBasket } from "../slices/basketSlice";
+import { useSelector } from "react-redux";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
 const Checkout = () => {
-  const dispatch = useDispatch();
+  const totalPrice = useSelector(selectTotal);
   const items = useSelector(selectItems);
   const router = useRouter();
   const session = useSession();
@@ -20,33 +21,45 @@ const Checkout = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-gray-100">
       <Header />
-      <main className="lg:flex max-w-screen-2xl mx-auto">
-        {/* Left Section */}
-        <div className="flex-grow my-5 shadow-sm">
-          <Image
-            src="https://links.papareact.com/ikj"
-            width={1020}
-            height={250}
-            objectFit="contain"
-          />
-        </div>
+      <main className="lg:flex max-w-screen-2xl mx-auto p-5">
+        <div>
+          {/* Left Section */}
+          <div className="flex-grow mb-5 shadow-sm">
+            <Image
+              src="https://links.papareact.com/ikj"
+              width={1020}
+              height={250}
+              objectFit="contain"
+            />
+          </div>
 
-        <div className="flex flex-col p-5 space-y-10 bg-white">
-          <h1 className="text-3xl border-b pb-4">
-            {items.length === 0
-              ? "No items on your shopping basket"
-              : "Your shopping basket"}
-          </h1>
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "No items on your shopping basket"
+                : "Your shopping basket"}
+            </h1>
 
-          {items.map((item, index) => (
-            <CheckoutProduct key={`CheckoutProduct-${index}`} {...item} />
-          ))}
+            {items.map((item, index) => (
+              <CheckoutProduct key={`CheckoutProduct-${index}`} {...item} />
+            ))}
+          </div>
         </div>
 
         {/* Right Section */}
-        <div></div>
+        {items.length > 0 && (
+          <div className="flex flex-col bg-white p-10 shadow-md lg:ml-5 lg:mt-0 sm:mt-5">
+            <h2 className="whitespace-nowrap">
+              Subtotal ({items.length} items):{" "}
+              <span className="font-bold">
+                <Currency quantity={totalPrice} currency="EUR" />
+              </span>
+            </h2>
+            <button className="button">Checkout</button>
+          </div>
+        )}
       </main>
     </div>
   );
